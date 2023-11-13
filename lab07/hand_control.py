c5b8ad09f8455c4d8f1529012371a9bf96c4eb5c
LinZC30 <1150609717@qq.com>
2023-11-13 16:35:00 +0800

    deleted some commented code

--- lab07/hand_control.py
+++ lab07/hand_control.py
@@ -22,7 +22,6 @@ cam = Picamera2()
 cam.still_configuration.main.size=(640,480)
 cam.still_configuration.main.format='RGB888'
 cam.configure("still")
-#cam = cv2.VideoCapture(0)
 
 
 # 神 经 网 络 同 训 练 时 的 网 络 结 构
@@ -48,17 +47,6 @@ class Network(nn.Module):
         t = F.relu(self.fc2(t))
         return self.out(t)
 
-
-# def pic(img):
-#     ret, frame = cv2.imread(img)
-#     HSV = cv2.cvtColor(frame , cv2.COLOR_BGR2HSV) # HSV颜 色 空 间 的 图 像
-#     gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY) # 灰 度 化 的 图 像
-#     image_mask = cv2.inRange(HSV, np.array([0, 0, 0]), np.array([50, 200, 160])) # 实 验 室 相 机 的 肤 色
-#     output = cv2.bitwise_and(gray, gray, mask=image_mask) # 按 颜 色 空 间 提 取 手
-#     output = cv2.resize(output , (80, 60)) # 缩 小 图 片
-#     output = cv2.blur(output , (2, 2)) # 模 糊 处 理
-#     return output
-
 cam.start()
 time.sleep(1)
 
@@ -66,8 +54,6 @@ network = torch.load('network.pkl')
 
 while True :
     frame = cam.capture_array('main')
-    #output = pic(img)
-    #ret, frame = cam.read()
     HSV = cv2.cvtColor(frame , cv2.COLOR_BGR2HSV) # HSV颜 色 空 间 的 图 像
     gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY) # 灰 度 化 的 图 像
     image_mask = cv2.inRange(HSV, np.array([2, 20, 30]), np.array([40, 200, 200])) # 实 验 室 相 机 的 肤 色
@@ -82,8 +68,6 @@ while True :
     print(pred_scores)
     prediction = pred_scores.argmax(dim=1).item() # 取 最 大 者 为 结 果
     print(prediction)
-#     cv2.imshow('img', img)
-#     cv2.imshow('output', output)
     if prediction == Five and value == 0:
         value = 200
     elif prediction == Palm:
@@ -97,10 +81,6 @@ while True :
     if cv2.waitKey(1) == ord("q"):
         
         break
-    #sleep(0.2)   
+       
 cv2.destroyAllWindows()
-#cam.release()
 cam.stop()
-# data = torch.tensor([[output]], dtype=torch.float) # 将 获 取 的 实 时 样 本 转 为 可 传 入 网络 的tensor
-# pred_scores = network(data) # 获 取 各 类 型 的 分 数
-# prediction = pred_scores.argmax(dim=1).item() # 取 最 大 者 为 结 果
